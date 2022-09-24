4becec170541a691bf470a5fb49c51f3b3530646
Divyansh Dubey <108198974+divyanshdube@users.noreply.github.com>
2022-09-24 13:28:46 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,11 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+      <header data-ns-test= "project-name" >E-Commerce Website</header>
+      <div data-ns-test= "project-name">This will provide user to shop across PAN India</div>
+      
+    </div>
   )
 }
 
